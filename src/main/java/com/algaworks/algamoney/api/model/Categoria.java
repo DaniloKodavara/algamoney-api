5aadc6d8c64d19808a00a7a4f959517ca9e6d6b0
danilo.kodavara@hotmail.com <danilo.kodavara@hotmail.com>
2021-09-23 19:37:12 -0300

    Criando Modelo de Categoria, e recurso do modelo. Criando metodos para listar categorias, salvar, e buscar por codigo.

---
+++ src/main/java/com/algaworks/algamoney/api/model/Categoria.java
@@ -0,0 +1,18 @@
+package com.algaworks.algamoney.api.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+}
---
+++ src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -0,0 +1,43 @@
+package com.algaworks.algamoney.api.resource;
+
+import com.algaworks.algamoney.api.model.Categoria;
+import com.algaworks.algamoney.api.repository.CategoriaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+    private final CategoriaRepository repository;
+
+    public CategoriaResource(CategoriaRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Categoria> listar() {
+        return this.repository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaSalva = this.repository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{codigo}").buildAndExpand(categoriaSalva.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+        return this.repository.findById(codigo).orElse(null);
+    }
+
+}
